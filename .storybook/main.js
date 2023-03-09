dc636d3d5d493966253273f3f3afb3990fde2773
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2023-03-09 20:12:06 +0000

    Bump storybook and tweak config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,14 @@
 module.exports = {
-  "addons": [
-    "@chakra-ui/storybook-addon",
-    "@storybook/addon-essentials",
-  ],
-  "features": {
-    "emotionAlias": false,
+  addons: ["@chakra-ui/storybook-addon", "@storybook/addon-essentials"],
+  features: {
+    emotionAlias: false
   },
-  "stories": [
-    "../examples/**/*.stories.mdx",
-  ],
-}
+  stories: ["../examples/**/*.stories.mdx"],
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack4',
+  },
+  docs: {
+    autodocs: true
+  }
+};
